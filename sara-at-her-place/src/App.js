279ee8d131ffcb052bceb4cae4f865d21c00f615
Cesar Lara <larachicharo@protonmail.com>
2020-09-25 20:24:22 -0500

    Adding more time units.

--- sara-at-her-place/src/App.js
+++ sara-at-her-place/src/App.js
@@ -3,19 +3,29 @@ import sara from './saraconda2.png';
 import './App.css';
 
 function App() {
-  const dateFirstInside = new Date('2020-04-21T11:00:00');
-  const days = Math.floor((Date.now() - dateFirstInside)/(1000*3600*24));
+  const timeFirstInside = new Date('March 21, 2020, 11:00:00 GMT-06:00');
+  const milliseconds = (Date.now() - timeFirstInside);
+  const seconds = milliseconds/1000;
+  const minutes = seconds/60;
+  const hours = minutes/60;
+  const days = hours/24;
+  const years = days/365;
   return (
     <div className="App">
       <header className="App-header">
         <p>This is Sara</p>
         <p>She is a covid <b>hero</b></p>
         <img src={sara} className="Sara-pic" alt="saraconda" />
-        <h2>
-          She hasn't left her house in exactly {days} days
-        </h2>
-        <p>(Don't) Be like Sara</p>
+        <h2>She hasn't left her house in exactly:</h2>
       </header>
+      <div className="App-body">
+        <h3 className="Item">{Math.floor(days)} days</h3>
+        <h3 className="Item">{Math.floor(hours)} hours</h3>
+        <h3 className="Item">{Math.floor(minutes)} minutes</h3>
+        <h3 className="Item">{Math.floor(seconds)} seconds</h3>
+        <h3 className="Item">{Math.floor(seconds)} seconds</h3>
+        <h2>(Don't) Be like Sara</h2>
+      </div>
     </div>
   );
 }
